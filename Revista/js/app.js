590196ff9ce27384d05d365bdcd6fb89f1da526f
Diego Vega <diegovr99@hotmail.com>
2019-11-06 11:58:56 -0600

    Se añadieron nuevos archivos de webpack (htmls, scss, etc)

--- Revista/js/app.js
+++ Revista/js/app.js
@@ -1,2 +1,4 @@
-import '../css/app.scss';
+import '../sass/app.scss';
+import '../sass/art1.scss';
+import './vendor/jquery';
 $(document).foundation()
--- Revista/webpack.config.js
+++ Revista/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry:{app: './js/app.js'},
@@ -9,15 +10,21 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.scss$/,
+                test: /\.css$/,
+                test: /\.scss$/,                
                 use: [
                     {loader: 'style-loader'},
                     {loader: 'css-loader'},
                     {loader: 'sass-loader'}
                 ]
-            }
+            }            
         ]
     },
+    externals: {
+        // require("jquery") is external and available
+        //  on the global var jQuery
+        "jquery": "jQuery"
+      },      
     plugins:[
         new HtmlWebpackPlugin({
             filename: 'index.html',
@@ -27,6 +34,61 @@ module.exports = {
             filename: 'articulo1.html',
             template: './html/articulo1.html',
             chunks: ['app']          
-        })        
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'articulo2.html',
+            template: './html/articulo2.html',
+            chunks: ['app']          
+        })            ,
+        new HtmlWebpackPlugin({
+            filename: 'articulo3.html',
+            template: './html/articulo3.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'categ1.html',
+            template: './html/categ1.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'categ2.html',
+            template: './html/categ2.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'categorias.html',
+            template: './html/categorias.html',
+            chunks: ['app']          
+        })   ,
+        new HtmlWebpackPlugin({
+            filename: 'crear_art.html',
+            template: './html/crear_art.html',
+            chunks: ['app']          
+        })     ,
+        new HtmlWebpackPlugin({
+            filename: 'login.html',
+            template: './html/login.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'modulo-anuncios.html',
+            template: './html/modulo-anuncios.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'modulo-articulo.html',
+            template: './html/modulo-articulo.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'modulo-moderacion.html',
+            template: './html/modulo-moderacion.html',
+            chunks: ['app']          
+        })    ,
+        new HtmlWebpackPlugin({
+            filename: 'Usuario.html',
+            template: './html/Usuario.html',
+            chunks: ['app']          
+        })    
     ]    
 }
